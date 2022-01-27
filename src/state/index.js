f6a9bf88fb1d6ac3c564063e35374270f11ee3a6
Rishikant Patel <38522693+082-Rishikant@users.noreply.github.com>
2022-01-27 20:51:20 +0530

    Lecture-76 Creating a Redux Store and Give its access to whole Application

---
+++ src/state/index.js
@@ -0,0 +1,2 @@
+// This index.js file is to export the action-creators from state so that we can easly export all action-creators from state
+export * as actionCreators from './action-creators/index'
